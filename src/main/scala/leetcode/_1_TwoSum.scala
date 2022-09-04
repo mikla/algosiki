b29ce00c903b3fe6225dedafd0dc212ccf9995df
Artsiom Miklushou <amiklushou@evolution.com>
2022-09-04 13:07:29 +0300

    Daily leetcode.

---
+++ src/main/scala/leetcode/_1_TwoSum.scala
@@ -0,0 +1,29 @@
+package leetcode
+
+// https://leetcode.com/problems/two-sum/
+object _1_TwoSum extends App {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+
+    val numsWithIndexes = nums.zipWithIndex.sortBy(_._1)
+
+    def searchIndex(left: Int, right: Int): List[Int] = {
+      if (left < right) {
+        val leftElem = numsWithIndexes(left)
+        val rightElem = numsWithIndexes(right)
+
+        val sum = leftElem._1 + rightElem._1
+        if (sum == target) List(leftElem._2, rightElem._2)
+        else if (sum > target) searchIndex(left, right - 1)
+        else searchIndex(left + 1, right)
+
+      } else Nil
+    }
+
+    searchIndex(0, numsWithIndexes.length - 1).toArray
+  }
+
+  println(_1_TwoSum.twoSum(Array(2,7,11,15), 9).toList)
+  println(_1_TwoSum.twoSum(Array(3,2,4), 6).toList)
+  println(_1_TwoSum.twoSum(Array(3,3), 6).toList)
+
+}
---
+++ src/main/scala/leetcode/_987_VerticalOrderTraversalOfABinaryTree.scala
@@ -0,0 +1,42 @@
+package leetcode
+
+// https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+object _987_VerticalOrderTraversalOfABinaryTree extends App {
+
+  object Solution {
+
+    case class Cord(row: Int, col: Int) {
+      def left: Cord = copy(row = row + 1, col = col - 1)
+
+      def right: Cord = copy(row = row + 1, col = col + 1)
+    }
+
+    def verticalTraversal(root: TreeNode): List[List[Int]] = {
+      val markedTreeValues = traverse(root, Cord(0, 0), List.empty)
+
+      markedTreeValues
+        .sortBy(r => (r._1.col, r._1.row, r._2))
+        .groupBy(_._1.col)
+        .toList
+        .sortBy(_._1)
+        .map(_._2.map(_._2))
+
+    }
+
+    def traverse(tree: TreeNode, currCord: Cord, acc: List[(Cord, Int)]): List[(Cord, Int)] = {
+      val currNode = acc :+ (currCord -> tree.value)
+
+      val leftTraverse = if (tree.left != null) traverse(tree.left, currCord.left, acc) else Nil
+      val rightTraverse = if (tree.right != null) traverse(tree.right, currCord.right, acc) else Nil
+
+      currNode ++ leftTraverse ++ rightTraverse
+    }
+  }
+
+  val testTree1 = new TreeNode(3,
+    new TreeNode(9),
+      new TreeNode(20, new TreeNode(15), new TreeNode(7)))
+
+  println(Solution.verticalTraversal(testTree1))
+
+}
