bf81642f1abf9620ec43a3f598133a052c56fc02
Artsiom Miklushou <amiklushou@evolution.com>
2022-09-19 21:34:05 +0300

    Leftist heap.

--- src/main/scala/pfds/chapter3/LeftistHeap.scala
+++ src/main/scala/pfds/chapter3/LeftistHeap.scala
@@ -1,11 +1,87 @@
 package pfds.chapter3
 
+import pfds.chapter3.LeftistHeap.Heap.{insert, merge}
+
 object LeftistHeap {
 
-  trait Heap[+T]
+  trait Heap[+T] {
+
+    def ins[V >: T](elem: V)(implicit O: Ordering[V]): Heap[V] =
+      insert(elem, this)
+
+    def findMin: T = this match {
+      case Branch(_, e, _, _) => e
+      case Empty => throw new RuntimeException("findMin on Empty heap.")
+    }
+
+    def deleteMin[V >: T](implicit O: Ordering[V]): Heap[V] = this match {
+      case Branch(_, _, left, right) => merge[V](left, right)
+      case Empty => throw new RuntimeException("deleteMin on Empty heap.")
+    }
+
+  }
+
+  object Heap {
+
+    // Exercise 3.3
+    // Not really efficient impl.
+    def fromList[T](list: List[T])(implicit O: Ordering[T]): Heap[T] =
+      list.foldLeft(Empty: Heap[T]) { case (heap, element) =>
+        heap.ins(element)
+      }
+
+    def print[T](h: Heap[T]): String = {
+
+      def loop(heap: Heap[T], level: Int): List[String] = heap match {
+        case Branch(rank, elem, left, right) =>
+          val c = " ".repeat(rank * 2) + s"($elem)" + " ".repeat(rank * 2)
+          val leftDel = " ".repeat(rank * 2) + "/ " + " ".repeat(rank * 2) + "\\"
+
+          val l = loop(left, level + 1)
+          val r = loop(right, level +1)
+          c :: leftDel :: l.zip(r).map { case (l1, r1) => l1 + r1 }
+
+        case Empty => List("  E  ")
+      }
+
+      loop(h, 0).mkString("\n")
+
+    }
+
+    def merge[T](h1: Heap[T], h2: Heap[T])(implicit O: Ordering[T]): Heap[T] =
+      (h1, h2) match {
+        case (Empty, h) => h
+        case (h, Empty) => h
+        case (
+              h1 @ Branch(_, x, a1, b1),
+              h2 @ Branch(_, y, a2, b2)
+            ) =>
+          if (O.lteq(x, y)) makeT(x, a1, merge(b1, h2))
+          else makeT(y, a2, merge(h1, b2))
+      }
+
+    def insert[T](elem: T, h1: Heap[T])(implicit O: Ordering[T]): Heap[T] =
+      merge(Branch(1, elem, Empty, Empty), h1)
+
+    // swaps children of node base on rank if necessary
+    private def makeT[T](elem: T, a: Heap[T], b: Heap[T]): Heap[T] =
+      if (rank(a) >= rank(b)) Branch(rank(b) + 1, elem, a, b)
+      else Branch(rank(a) + 1, elem, b, a)
+
+    private def rank[T](node: Heap[T]): Int = node match {
+      case Branch(rank, _, _, _) => rank
+      case Empty                 => 0
+    }
+
+  }
+
   case object Empty extends Heap[Nothing]
-  case class Leaf[T](elem: T, left: Heap[T], right: Heap[T]) extends Heap[T]
 
-
+  case class Branch[T](
+      rank: Int,
+      elem: T,
+      left: Heap[T] = Empty,
+      right: Heap[T] = Empty
+  ) extends Heap[T]
 
 }
---
+++ src/test/scala/pfds/chapter3/LeftistHeapSpec.scala
@@ -0,0 +1,32 @@
+package pfds.chapter3
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+import pfds.chapter3.LeftistHeap.{Branch, Heap, Empty}
+
+class LeftistHeapSpec extends AnyFlatSpec with should.Matchers {
+
+  "Heap.fromList" should "make a heap" in {
+
+    val heap = Heap.fromList((1 to 10).toList)
+
+    val tree = Branch(
+      rank = 3,
+      elem = 1,
+      left =
+        Branch(2, 3, Branch(1, 4, Empty, Empty), Branch(1, 5, Empty, Empty)),
+      right = Branch(
+        rank = 2,
+        elem = 2,
+        left =
+          Branch(2, 7, Branch(1, 8, Empty, Empty), Branch(1, 9, Empty, Empty)),
+        right = Branch(1, 6, Branch(1, 10, Empty, Empty), Empty)
+      )
+    )
+
+    println(Heap.print(heap))
+
+    assert(heap == tree)
+  }
+
+}
