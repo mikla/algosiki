168b0c40cd634b66850c38ab98361f56b088459a
Artsiom Miklushou <amiklushou@evolution.com>
2022-09-23 01:38:41 +0300

    leetcode daily

---
+++ src/main/scala/leetcode/_541_ReversString2.scala
@@ -0,0 +1,21 @@
+package leetcode
+
+object _541_ReversString2 extends App {
+
+  object Solution {
+    def reverseStr(s: String, k: Int): String = {
+      s.sliding(k, k)
+        .toList
+        .zipWithIndex
+        .map { case (str, index) =>
+          if (index % 2 == 0) str.reverse
+          else str
+        }
+        .mkString("")
+    }
+  }
+
+  println(Solution.reverseStr("abcdefg", 2))
+  println(Solution.reverseStr("abcd", 2))
+
+}
