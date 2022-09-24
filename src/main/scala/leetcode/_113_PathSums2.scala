17595c8ec3cab434d49c0886b1ba989c074a2a1b
Artsiom Miklushou <amiklushou@evolution.com>
2022-09-25 00:35:37 +0300

    leetcode daily

---
+++ src/main/scala/leetcode/_113_PathSums2.scala
@@ -0,0 +1,52 @@
+package leetcode
+
+object _113_PathSums2 extends App {
+
+  object Solution {
+
+    def isLeaf(treeNode: TreeNode): Boolean =
+      treeNode.left == null && treeNode.right == null
+
+    def pathSum(root: TreeNode, targetSum: Int): List[List[Int]] = {
+
+      def loop(
+          node: TreeNode,
+          currentSum: Int,
+          path: List[Int]
+      ): List[List[Int]] = {
+        if (node != null) {
+          val recur = loop(_, currentSum + node.value, path :+ node.value)
+          if (currentSum + node.value == targetSum && isLeaf(node)) {
+            List(path :+ node.value)
+          } else recur(node.left) ++ recur(node.right)
+        } else Nil
+      }
+
+      loop(root, 0, Nil)
+
+    }
+
+  }
+
+  val testTree1 =
+    new TreeNode(
+      5,
+      new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))),
+      new TreeNode(
+        8,
+        new TreeNode(13),
+        new TreeNode(4, new TreeNode(5), new TreeNode(1))
+      )
+    )
+
+  val testTree2 =
+    new TreeNode(
+      -2,
+      null,
+      new TreeNode(-3)
+    )
+
+
+  println(Solution.pathSum(testTree1, 22))
+  println(Solution.pathSum(testTree2, -5))
+}
--- src/main/scala/leetcode/package.scala
+++ src/main/scala/leetcode/package.scala
@@ -16,4 +16,10 @@ package object leetcode {
     var children: List[Node] = List()
   }
 
+  class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+
+  }
+
 }
