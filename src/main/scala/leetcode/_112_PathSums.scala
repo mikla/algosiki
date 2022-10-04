038068e6dba5192d868e93aa3088932a0984d458
Artsiom Miklushou <amiklushou@evolution.com>
2022-10-04 15:15:19 +0300

    112 Daily leetcode.

---
+++ src/main/scala/leetcode/_112_PathSums.scala
@@ -0,0 +1,50 @@
+package leetcode
+
+object _112_PathSums extends App {
+
+  object Solution {
+
+    def isLeaf(treeNode: TreeNode): Boolean =
+      treeNode.left == null && treeNode.right == null
+
+    def hasPathSum(root: TreeNode, targetSum: Int): Boolean = {
+
+      def loop(
+          node: TreeNode,
+          currentSum: Int,
+          path: List[Int]
+      ): List[List[Int]] = {
+        if (node != null) {
+          val recur = loop(_, currentSum + node.value, path :+ node.value)
+          if (currentSum + node.value == targetSum && isLeaf(node)) {
+            List(path :+ node.value)
+          } else recur(node.left) ++ recur(node.right)
+        } else Nil
+      }
+
+      loop(root, 0, Nil).nonEmpty
+    }
+
+  }
+
+  val testTree1 =
+    new TreeNode(
+      5,
+      new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))),
+      new TreeNode(
+        8,
+        new TreeNode(13),
+        new TreeNode(4, new TreeNode(5), new TreeNode(1))
+      )
+    )
+
+  val testTree2 =
+    new TreeNode(
+      -2,
+      null,
+      new TreeNode(-3)
+    )
+
+  println(Solution.hasPathSum(testTree1, 22))
+  println(Solution.hasPathSum(testTree2, -5))
+}
