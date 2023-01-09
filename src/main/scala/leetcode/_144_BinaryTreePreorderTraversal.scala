bc04725e396dd833438bf6fd06373f89905dfc4c
Artsiom Miklushou <mikla.op@gmail.com>
2023-01-10 00:34:44 +0200

    Leetcode daily.

---
+++ src/main/scala/leetcode/_144_BinaryTreePreorderTraversal.scala
@@ -0,0 +1,18 @@
+package leetcode
+
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+object _144_BinaryTreePreorderTraversal {
+
+  def preorderTraversal(root: TreeNode): List[Int] = {
+
+    def loop(tree: TreeNode): List[Int] = {
+      if (tree == null) Nil
+      else
+        List(tree.value) ++ loop(tree.left) ++ loop(tree.right)
+    }
+
+    loop(root)
+
+  }
+
+}
---
+++ src/main/scala/leetcode/_149_MaxPointsOnALine.scala
@@ -0,0 +1,40 @@
+package leetcode
+
+import scala.util.control.Breaks.break
+
+object _149_MaxPointsOnALine extends App {
+
+  def maxPoints(points: Array[Array[Int]]): Int = {
+
+    if (points.length == 1) 1
+    else {
+
+      var i = 0;
+      var max = 0;
+      while (i < points.length) {
+        var j = i + 1;
+        val Array(p1x, p1y) = points(i)
+        while (j < points.length) {
+          val Array(p2x, p2y) = points(j)
+
+          val lineEquation = lineEq((p1x, p1y), (p2x, p2y)) _
+          val pointsOnLine = points.count { case Array(x, y) =>
+            lineEquation((x, y))
+          }
+
+          if (pointsOnLine > max) max = pointsOnLine
+
+          j += 1
+        }
+
+        i += 1
+      }
+
+      max
+    }
+  }
+
+  def lineEq(p1: (Int, Int), p2: (Int, Int))(px: (Int, Int)): Boolean =
+    (px._1 - p1._1) * (p2._2 - p1._2) == (px._2 - p1._2) * (p2._1 - p1._1)
+
+}
