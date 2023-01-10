bcd21e4eeef3c4b9fc3263f8beb3857e0c587638
Artsiom Miklushou <mikla.op@gmail.com>
2023-01-10 20:48:20 +0200

    Leetcode daily, 520

---
+++ src/main/scala/leetcode/_520_DetectCapital.scala
@@ -0,0 +1,12 @@
+package leetcode
+
+object _520_DetectCapital extends App {
+
+  def detectCapitalUse(word: String): Boolean = {
+    word.toLowerCase == word || word.toUpperCase == word || {
+      word.head.toUpper == word.head.toUpper && word.tail.toLowerCase == word.tail
+    }
+
+  }
+
+}
