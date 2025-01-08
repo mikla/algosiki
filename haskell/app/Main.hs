1f6955cc211291ea659f8fbbb3cfb500e993f24d
amiklushou <amiklushou@evolution.com>
2025-01-08 23:29:24 +0200

    Setup haskell project to experiment with fgl library.

---
+++ haskell/app/Main.hs
@@ -0,0 +1,25 @@
+module Main (main) where
+
+import Data.Graph.Inductive
+
+myGraph :: Gr String String
+myGraph = mkGraph nodes edges
+  where
+    nodes =
+      [ (1, "a"),
+        (2, "b"),
+        (3, "c")
+      ]
+    edges =
+      [ (1, 2, "right"),
+        (2, 3, "down"),
+        (3, 1, "up"),
+        (2, 1, "left")
+      ]
+
+main :: IO ()
+main = do
+  -- Run DFS starting from node 1
+  let dfsNodes = dfs [3] myGraph
+  putStrLn "DFS traversal order:"
+  print dfsNodes
